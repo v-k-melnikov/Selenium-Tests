28433947dcc2d408e0bfbcd33622347c45373103
v-k-melnikov <v.k.melnikov.92@gmail.com>
2018-09-20 19:13:13 +0300

    Exersize 17

--- src/test/java/com/LogTestEx17.java
+++ src/test/java/com/LogTestEx17.java
@@ -5,9 +5,12 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class LogTestEx17 extends TestBase {
 
@@ -19,22 +22,28 @@ public class LogTestEx17 extends TestBase {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         login();
         driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1");
+
+        // opening all folders
+
         try {
             while (true) {
                 driver.findElement(By.xpath("//td[i[(@class='fa fa-folder')]]/a")).click();
             }
         } catch (NoSuchElementException ignored) {
         }
+
+        // opening all products
+
         ArrayList<WebElement> list = new ArrayList<>(driver.findElements(By.xpath("//tbody/tr/td/a[not(@title='Edit')]")));
         String mainWindow = driver.getWindowHandle();
         for (WebElement el : list) {
             el.sendKeys(selectLinkOpeninNewTab);
             String newWindow = wait.until(thereIsWindowOtherThan(mainWindow));
             driver.switchTo().window(newWindow);
-            driver.findElement(By.cssSelector("[name='save']"));
+            driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+            Assert.assertEquals(0, driver.manage().logs().get("browser").getAll().size());
             driver.close();
             driver.switchTo().window(mainWindow);
         }
-        driver.manage().logs().get("browser").getAll().forEach(System.out::println);
     }
 }
